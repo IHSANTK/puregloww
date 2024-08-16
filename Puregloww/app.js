a28438d1db4eb6e10a015c03362cd0991f0d2788
MUHAMMAD IHSAN TK <150010046+IHSANTK@users.noreply.github.com>
2024-08-16 10:26:06 +0530

    Update app.js

--- Puregloww/app.js
+++ Puregloww/app.js
@@ -47,7 +47,7 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://puregloww.onrender.com/auth/google/callback"
+    callbackURL:  process.env.GOOGLE_CALLBACK_URL
 },
     async (accessToken, refreshToken, profile, done) => {
         try {
